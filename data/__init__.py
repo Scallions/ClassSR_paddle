b7410da81134de2a1f75af47601a5089f8a09cfe
yifwahaha <49463907+yifwahaha@users.noreply.github.com>
2021-08-24 16:57:50 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,14 +1,15 @@
 """create dataset and dataloader"""
 import logging
-import torch
-import torch.utils.data
+import paddle
+from paddle.io import Dataset,DataLoader
 
 
 def create_dataloader(dataset, dataset_opt, opt=None, sampler=None):
     phase = dataset_opt['phase']
+    #TODO distributed training
     if phase == 'train':
         if opt['dist']:
-            world_size = torch.distributed.get_world_size()
+            world_size = paddle.distributed.get_world_size()
             num_workers = dataset_opt['n_workers']
             assert dataset_opt['batch_size'] % world_size == 0
             batch_size = dataset_opt['batch_size'] // world_size
@@ -17,37 +18,38 @@ def create_dataloader(dataset, dataset_opt, opt=None, sampler=None):
             num_workers = dataset_opt['n_workers'] * len(opt['gpu_ids'])
             batch_size = dataset_opt['batch_size']
             shuffle = True
-        return torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
-                                           num_workers=num_workers, sampler=sampler, drop_last=True,
-                                           pin_memory=False)
+        return paddle.io.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+                                           num_workers=num_workers, drop_last=True)
     else:
-        return torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1,
-                                           pin_memory=False)
+        return paddle.io.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=1)
 
 
-def create_dataset(dataset_opt):
-    mode = dataset_opt['mode']
-    # datasets for image restoration
-    if mode == 'LQ':
-        from data.LQ_dataset import LQDataset as D
-    elif mode == 'LQGT':
-        from data.LQGT_dataset import LQGTDataset as D
-    elif mode == 'LQGT_rcan':
-        from data.LQGT_rcan_dataset import LQGTDataset_rcan as D
-    elif mode == 'LQ_label':
-        from data.LQ_label_dataset import LQ_label_Dataset as D
-    # datasets for video restoration
-    elif mode == 'REDS':
-        from data.REDS_dataset import REDSDataset as D
-    elif mode == 'Vimeo90K':
-        from data.Vimeo90K_dataset import Vimeo90KDataset as D
-    elif mode == 'video_test':
-        from data.video_test_dataset import VideoTestDataset as D
-    else:
-        raise NotImplementedError('Dataset [{:s}] is not recognized.'.format(mode))
-    dataset = D(dataset_opt)
 
-    logger = logging.getLogger('base')
-    logger.info('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__,
-                                                           dataset_opt['name']))
-    return dataset
+
+def create_dataloader(dataset):
+    def create_dataset(dataset_opt):
+        mode = dataset_opt['mode']
+        # datasets for image restoration
+        if mode == 'LQ':
+            from data.LQ_dataset import LQDataset as D
+        elif mode == 'LQGT':
+            from data.LQGT_dataset import LQGTDataset as D
+        elif mode == 'LQGT_rcan':
+            from data.LQGT_rcan_dataset import LQGTDataset_rcan as D
+        elif mode == 'LQ_label':
+            from data.LQ_label_dataset import LQ_label_Dataset as D
+        # datasets for video restoration
+        elif mode == 'REDS':
+            from data.REDS_dataset import REDSDataset as D
+        elif mode == 'Vimeo90K':
+            from data.Vimeo90K_dataset import Vimeo90KDataset as D
+        elif mode == 'video_test':
+            from data.video_test_dataset import VideoTestDataset as D
+        else:
+            raise NotImplementedError('Dataset [{:s}] is not recognized.'.format(mode))
+        dataset = D(dataset_opt)
+
+        logger = logging.getLogger('base')
+        logger.info('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__,
+                                                               dataset_opt['name']))
+        return dataset
