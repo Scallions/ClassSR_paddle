f4c3063d8c3bf077bc88d5d22b763310e723ff5d
yifwahaha <49463907+yifwahaha@users.noreply.github.com>
2021-08-24 17:06:04 +0800

    Update util.py

--- data/util.py
+++ data/util.py
@@ -4,7 +4,7 @@ import pickle
 import random
 import numpy as np
 import glob
-import torch
+import paddle
 import cv2
 
 ####################
@@ -121,7 +121,7 @@ def read_img_seq(path):
     # stack to Torch tensor
     imgs = np.stack(img_l, axis=0)
     imgs = imgs[:, :, :, [2, 1, 0]]
-    imgs = torch.from_numpy(np.ascontiguousarray(np.transpose(imgs, (0, 3, 1, 2)))).float()
+    imgs = paddle.to_tensor(np.ascontiguousarray(np.transpose(imgs, (0, 3, 1, 2))),paddle.float32)
     return imgs
 
 
@@ -336,7 +336,7 @@ def modcrop(img_in, scale):
 
 # matlab 'imresize' function, now only support 'bicubic'
 def cubic(x):
-    absx = torch.abs(x)
+    absx = paddle.abs(x)
     absx2 = absx**2
     absx3 = absx**3
     return (1.5 * absx3 - 2.5 * absx2 + 1) * (
@@ -350,7 +350,8 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
         kernel_width = kernel_width / scale
 
     # Output-space coordinates
-    x = torch.linspace(1, out_length, out_length)
+    #TODO check paddle linspace with torch linspace
+    x = paddle.linspace(1, out_length, out_length)
 
     # Input-space coordinates. Calculate the inverse mapping such that 0.5
     # in output space maps to 0.5 in input space, and 0.5+scale in output
@@ -358,7 +359,7 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
     u = x / scale + 0.5 * (1 - 1 / scale)
 
     # What is the left-most pixel that can be involved in the computation?
-    left = torch.floor(u - kernel_width / 2)
+    left = paddle.floor(u - kernel_width / 2)
 
     # What is the maximum number of pixels that can be involved in the
     # computation?  Note: it's OK to use an extra pixel here; if the
@@ -368,7 +369,7 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
 
     # The indices of the input pixels involved in computing the k-th output
     # pixel are in row k of the indices matrix.
-    indices = left.view(out_length, 1).expand(out_length, P) + torch.linspace(0, P - 1, P).view(
+    indices = left.view(out_length, 1).expand(out_length, P) + paddle.linspace(0, P - 1, P).view(
         1, P).expand(out_length, P)
 
     # The weights used to compute the k-th output pixel are in row k of the
@@ -380,19 +381,23 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
     else:
         weights = cubic(distance_to_center)
     # Normalize the weights matrix so that each row sums to 1.
-    weights_sum = torch.sum(weights, 1).view(out_length, 1)
+    weights_sum = paddle.sum(weights, 1).view(out_length, 1)
     weights = weights / weights_sum.expand(out_length, P)
 
     # If a column in weights is all zero, get rid of it. only consider the first and last column.
-    weights_zero_tmp = torch.sum((weights == 0), 0)
+    weights_zero_tmp = paddle.sum((weights == 0), 0)
     if not math.isclose(weights_zero_tmp[0], 0, rel_tol=1e-6):
-        indices = indices.narrow(1, 1, P - 2)
-        weights = weights.narrow(1, 1, P - 2)
+        indices = paddle.slice(indices,[1], 1, P - 2)
+        #indices = indices.narrow(1, 1, P - 2)
+        weights = paddle.slice(weights,[1],1,P-2)
+        #weights = weights.narrow(1, 1, P - 2)
     if not math.isclose(weights_zero_tmp[-1], 0, rel_tol=1e-6):
-        indices = indices.narrow(1, 0, P - 2)
-        weights = weights.narrow(1, 0, P - 2)
-    weights = weights.contiguous()
-    indices = indices.contiguous()
+        indices = paddle.slice(indices, [1], 0, P - 2)
+        #indices = indices.narrow(1, 0, P - 2)
+        weights = paddle.slice(weights, [1], 0, P - 2)
+        #weights = weights.narrow(1, 0, P - 2)
+    #weights = weights.contiguous()
+    #indices = indices.contiguous()
     sym_len_s = -indices.min() + 1
     sym_len_e = indices.max() - in_length
     indices = indices + sym_len_s - 1
@@ -421,20 +426,24 @@ def imresize(img, scale, antialiasing=True):
         in_W, out_W, scale, kernel, kernel_width, antialiasing)
     # process H dimension
     # symmetric copying
-    img_aug = torch.FloatTensor(in_C, in_H + sym_len_Hs + sym_len_He, in_W)
-    img_aug.narrow(1, sym_len_Hs, in_H).copy_(img)
+    img_aug = paddle.zeros((in_C, in_H + sym_len_Hs + sym_len_He, in_W), paddle.float32)
+    img_aug = paddle.slice(img_aug,[1],sym_len_Hs,in_H).copy_(img)
+    #img_aug.narrow(1, sym_len_Hs, in_H).copy_(img)
 
     sym_patch = img[:, :sym_len_Hs, :]
-    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    # TODO check long type
+    inv_idx = paddle.arange(sym_patch.size(1) - 1, -1, -1).astype('long')
     sym_patch_inv = sym_patch.index_select(1, inv_idx)
-    img_aug.narrow(1, 0, sym_len_Hs).copy_(sym_patch_inv)
+    img_aug = paddle.slice(img_aug,[1],0,sym_len_Hs).copy_(sym_patch_inv)
+    #img_aug.narrow(1, 0, sym_len_Hs).copy_(sym_patch_inv)
 
     sym_patch = img[:, -sym_len_He:, :]
-    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(1) - 1, -1, -1).astype('long')
     sym_patch_inv = sym_patch.index_select(1, inv_idx)
-    img_aug.narrow(1, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
-
-    out_1 = torch.FloatTensor(in_C, out_H, in_W)
+    img_aug = paddle.slice(img_aug,[1],sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
+    #img_aug.narrow(1, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
+    # TODO mabye there is a bug
+    out_1 = paddle.to_tensor(in_C, out_H, in_W)
     kernel_width = weights_H.size(1)
     for i in range(out_H):
         idx = int(indices_H[i][0])
@@ -444,20 +453,21 @@ def imresize(img, scale, antialiasing=True):
 
     # process W dimension
     # symmetric copying
-    out_1_aug = torch.FloatTensor(in_C, out_H, in_W + sym_len_Ws + sym_len_We)
-    out_1_aug.narrow(2, sym_len_Ws, in_W).copy_(out_1)
+    out_1_aug = paddle.zeros((in_C, out_H, in_W + sym_len_Ws + sym_len_We),paddle.float32)
+    out_1_aug = paddle.slice(out_1_aug,[2],sym_len_Ws,in_W).copy_(out_1)
+    #out_1_aug.narrow(2, sym_len_Ws, in_W).copy_(out_1)
 
     sym_patch = out_1[:, :, :sym_len_Ws]
-    inv_idx = torch.arange(sym_patch.size(2) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(2) - 1, -1, -1).astype('float32')
     sym_patch_inv = sym_patch.index_select(2, inv_idx)
     out_1_aug.narrow(2, 0, sym_len_Ws).copy_(sym_patch_inv)
 
     sym_patch = out_1[:, :, -sym_len_We:]
-    inv_idx = torch.arange(sym_patch.size(2) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(2) - 1, -1, -1).long()
     sym_patch_inv = sym_patch.index_select(2, inv_idx)
     out_1_aug.narrow(2, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
 
-    out_2 = torch.FloatTensor(in_C, out_H, out_W)
+    out_2 = paddle.to_tensor(in_C, out_H, out_W)
     kernel_width = weights_W.size(1)
     for i in range(out_W):
         idx = int(indices_W[i][0])
@@ -467,12 +477,12 @@ def imresize(img, scale, antialiasing=True):
 
     return out_2
 
-
+# TODO paddle.slice == torch.narrow
 def imresize_np(img, scale, antialiasing=True):
     # Now the scale should be the same for H and W
     # input: img: Numpy, HWC BGR [0,1]
     # output: HWC BGR [0,1] w/o round
-    img = torch.from_numpy(img)
+    img = paddle.to_tensor(img)
 
     in_H, in_W, in_C = img.size()
     _, out_H, out_W = in_C, math.ceil(in_H * scale), math.ceil(in_W * scale)
@@ -491,20 +501,22 @@ def imresize_np(img, scale, antialiasing=True):
         in_W, out_W, scale, kernel, kernel_width, antialiasing)
     # process H dimension
     # symmetric copying
-    img_aug = torch.FloatTensor(in_H + sym_len_Hs + sym_len_He, in_W, in_C)
+    img_aug = paddle.zeros((in_H + sym_len_Hs + sym_len_He, in_W, in_C),paddle.float32)
+    #img_aug = torch.FloatTensor(in_H + sym_len_Hs + sym_len_He, in_W, in_C)
     img_aug.narrow(0, sym_len_Hs, in_H).copy_(img)
 
     sym_patch = img[:sym_len_Hs, :, :]
-    inv_idx = torch.arange(sym_patch.size(0) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(0) - 1, -1, -1).astype('float32')
     sym_patch_inv = sym_patch.index_select(0, inv_idx)
     img_aug.narrow(0, 0, sym_len_Hs).copy_(sym_patch_inv)
 
     sym_patch = img[-sym_len_He:, :, :]
-    inv_idx = torch.arange(sym_patch.size(0) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(0) - 1, -1, -1).astype('float32')
     sym_patch_inv = sym_patch.index_select(0, inv_idx)
     img_aug.narrow(0, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
 
-    out_1 = torch.FloatTensor(out_H, in_W, in_C)
+    #out_1 = torch.FloatTensor(out_H, in_W, in_C)
+    out_1 = paddle.zeros((out_H,in_W,in_C),paddle.float32)
     kernel_width = weights_H.size(1)
     for i in range(out_H):
         idx = int(indices_H[i][0])
@@ -514,20 +526,22 @@ def imresize_np(img, scale, antialiasing=True):
 
     # process W dimension
     # symmetric copying
-    out_1_aug = torch.FloatTensor(out_H, in_W + sym_len_Ws + sym_len_We, in_C)
+    #out_1_aug = torch.FloatTensor(out_H, in_W + sym_len_Ws + sym_len_We, in_C)
+    out_1_aug = paddle.zeros((out_H, in_W + sym_len_Ws + sym_len_We, in_C),paddle.float32)
     out_1_aug.narrow(1, sym_len_Ws, in_W).copy_(out_1)
 
     sym_patch = out_1[:, :sym_len_Ws, :]
-    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(1) - 1, -1, -1).astype('float32')
     sym_patch_inv = sym_patch.index_select(1, inv_idx)
     out_1_aug.narrow(1, 0, sym_len_Ws).copy_(sym_patch_inv)
 
     sym_patch = out_1[:, -sym_len_We:, :]
-    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    inv_idx = paddle.arange(sym_patch.size(1) - 1, -1, -1).astype('float32')
     sym_patch_inv = sym_patch.index_select(1, inv_idx)
     out_1_aug.narrow(1, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
 
-    out_2 = torch.FloatTensor(out_H, out_W, in_C)
+    out_2 = paddle.zeros((out_H, out_W, in_C),paddle.float32)
+    #out_2 = torch.FloatTensor(out_H, out_W, in_C)
     kernel_width = weights_W.size(1)
     for i in range(out_W):
         idx = int(indices_W[i][0])
@@ -536,8 +550,3 @@ def imresize_np(img, scale, antialiasing=True):
         out_2[:, i, 2] = out_1_aug[:, idx:idx + kernel_width, 2].mv(weights_W[i])
 
     return out_2.numpy()
-
-
-
-
-
