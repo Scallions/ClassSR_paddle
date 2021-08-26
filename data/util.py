00791de2d872d4a1917d623549a35ee90809af76
Scalliosn <844120806@qq.com>
2021-08-26 20:43:31 +0800

    update data augmentation

--- data/util.py
+++ data/util.py
@@ -340,8 +340,9 @@ def cubic(x):
     absx2 = absx**2
     absx3 = absx**3
     return (1.5 * absx3 - 2.5 * absx2 + 1) * (
-        (absx <= 1).type_as(absx)) + (-0.5 * absx3 + 2.5 * absx2 - 4 * absx + 2) * ((
-            (absx > 1) * (absx <= 2)).type_as(absx))
+        (absx <= 1).astype(absx.dtype)) + (-0.5 * absx3 + 2.5 * absx2 - 4 * absx + 2) * ((
+            (absx > 1).astype(absx.dtype) * (absx <= 2).astype(absx.dtype)).astype(absx.dtype))
+
 
 
 def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width, antialiasing):
@@ -369,29 +370,29 @@ def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width
 
     # The indices of the input pixels involved in computing the k-th output
     # pixel are in row k of the indices matrix.
-    indices = left.view(out_length, 1).expand(out_length, P) + paddle.linspace(0, P - 1, P).view(
-        1, P).expand(out_length, P)
+    indices = left.reshape([out_length, 1]).expand([out_length, P]) + paddle.linspace(0, P - 1, P).reshape([
+        1, P]).expand([out_length, P])
 
     # The weights used to compute the k-th output pixel are in row k of the
     # weights matrix.
-    distance_to_center = u.view(out_length, 1).expand(out_length, P) - indices
+    distance_to_center = u.reshape([out_length, 1]).expand([out_length, P]) - indices
     # apply cubic kernel
     if (scale < 1) and (antialiasing):
         weights = scale * cubic(distance_to_center * scale)
     else:
         weights = cubic(distance_to_center)
     # Normalize the weights matrix so that each row sums to 1.
-    weights_sum = paddle.sum(weights, 1).view(out_length, 1)
-    weights = weights / weights_sum.expand(out_length, P)
+    weights_sum = paddle.sum(weights, 1).reshape([out_length, 1])
+    weights = weights / weights_sum.expand([out_length, P])
 
     # If a column in weights is all zero, get rid of it. only consider the first and last column.
     weights_zero_tmp = paddle.sum((weights == 0), 0)
-    if not math.isclose(weights_zero_tmp[0], 0, rel_tol=1e-6):
+    if not math.isclose(weights_zero_tmp.numpy()[0], 0, rel_tol=1e-6):
         indices = paddle.slice(indices,[1], [1], [P - 2])
         #indices = indices.narrow(1, 1, P - 2)
         weights = paddle.slice(weights,[1],[1],[P-2])
         #weights = weights.narrow(1, 1, P - 2)
-    if not math.isclose(weights_zero_tmp[-1], 0, rel_tol=1e-6):
+    if not math.isclose(weights_zero_tmp.numpy()[-1], 0, rel_tol=1e-6):
         indices = paddle.slice(indices, [1], [0],[P - 2])
         #indices = indices.narrow(1, 0, P - 2)
         weights = paddle.slice(weights, [1], [0], [P - 2])
@@ -427,21 +428,24 @@ def imresize(img, scale, antialiasing=True):
     # process H dimension
     # symmetric copying
     img_aug = paddle.zeros((in_C, in_H + sym_len_Hs + sym_len_He, in_W), paddle.float32)
-    img_aug = paddle.slice(img_aug,[1],[sym_len_Hs],[in_H]).copy_(img)
+    # img_aug = paddle.slice(img_aug,[1],[sym_len_Hs],[in_H]).copy_(img)
+    img_aug[:,sym_len_Hs:sym_len_Hs+in_H,:] = img.clone()
     #img_aug.narrow(1, sym_len_Hs, in_H).copy_(img)
 
     sym_patch = img[:, :sym_len_Hs, :]
     # TODO: check long type
     inv_idx = paddle.arange(sym_patch.shape[1] - 1, -1, -1).astype('long')
-    sym_patch_inv = sym_patch.index_select(1, inv_idx)
-    img_aug = paddle.slice(img_aug,[1],[0],[sym_len_Hs]).copy_(sym_patch_inv)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 1)
+    # img_aug = paddle.slice(img_aug,[1],[0],[sym_len_Hs]).copy_(sym_patch_inv)
     #img_aug.narrow(1, 0, sym_len_Hs).copy_(sym_patch_inv)
+    img_aug[:,:sym_len_Hs,:] = sym_patch_inv.clone()
 
     sym_patch = img[:, -sym_len_He:, :]
     inv_idx = paddle.arange(sym_patch.shape[1] - 1, -1, -1).astype('long')
-    sym_patch_inv = sym_patch.index_select(1, inv_idx)
-    img_aug = paddle.slice(img_aug,[1],[sym_len_Hs + in_H], [sym_len_He]).copy_(sym_patch_inv)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 1)
+    # img_aug = paddle.slice(img_aug,[1],[sym_len_Hs + in_H], [sym_len_He]).copy_(sym_patch_inv)
     #img_aug.narrow(1, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
+    img_aug[:,sym_len_Hs+in_H:sym_len_Hs+in_H+sym_len_He,] = sym_patch_inv.clone()
     # TODO: mabye there is a bug
     out_1 = paddle.to_tensor(in_C, out_H, in_W)
     kernel_width = weights_H.shape[1]
@@ -459,15 +463,17 @@ def imresize(img, scale, antialiasing=True):
 
     sym_patch = out_1[:, :, :sym_len_Ws]
     inv_idx = paddle.arange(sym_patch.shape[2] - 1, -1, -1).astype('float32')
-    sym_patch_inv = sym_patch.index_select(2, inv_idx)
-    out_1_aug = paddle.slice(out_1_aug,[2],[0],[sym_len_Ws]).copy_(sym_patch_inv)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 2)
+    # out_1_aug = paddle.slice(out_1_aug,[2],[0],[sym_len_Ws]).copy_(sym_patch_inv)
     #out_1_aug.narrow(2, 0, sym_len_Ws).copy_(sym_patch_inv)
+    out_1_aug[:,:,:sym_len_Ws] = sym_patch_inv.clone()
 
     sym_patch = out_1[:, :, -sym_len_We:]
     inv_idx = paddle.arange(sym_patch.shape[2] - 1, -1, -1).astype('float32')
-    sym_patch_inv = sym_patch.index_select(2, inv_idx)
-    out_1_aug = paddle.slice(out_1_aug,[2],[sym_len_Ws + in_W], [sym_len_We]).copy_(sym_patch_inv)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 2)
+    # out_1_aug = paddle.slice(out_1_aug,[2],[sym_len_Ws + in_W], [sym_len_We]).copy_(sym_patch_inv)
     #out_1_aug.narrow(2, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
+    out_1_aug[:,:,sym_len_Ws+in_W:sym_len_Ws+in_W+sym_len_We] = sym_patch_inv.clone()
 
     out_2 = paddle.to_tensor(in_C, out_H, out_W)
     kernel_width = weights_W.shape[1]
@@ -505,42 +511,48 @@ def imresize_np(img, scale, antialiasing=True):
     # symmetric copying
     img_aug = paddle.zeros((in_H + sym_len_Hs + sym_len_He, in_W, in_C),paddle.float32)
     #img_aug = torch.FloatTensor(in_H + sym_len_Hs + sym_len_He, in_W, in_C)
-    img_aug.narrow(0, sym_len_Hs, in_H).copy_(img)
+    # img_aug.narrow(0, sym_len_Hs, in_H).copy_(img)
+    img_aug[sym_len_He:sym_len_He+in_H,:,:] = img.astype("float32")
 
-    sym_patch = img[:sym_len_Hs, :, :]
-    inv_idx = paddle.arange(sym_patch.shape[0] - 1, -1, -1).astype('float32')
-    sym_patch_inv = sym_patch.index_select(0, inv_idx)
-    img_aug.narrow(0, 0, sym_len_Hs).copy_(sym_patch_inv)
+    sym_patch = img.astype('float32')[:sym_len_Hs, :, :]
+    inv_idx = paddle.arange(sym_patch.shape[0] - 1, -1, -1)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 0)
+    # img_aug.narrow(0, 0, sym_len_Hs).copy_(sym_patch_inv)
+    img_aug[:sym_len_Hs,:,:] = sym_patch_inv.astype("float32")
 
-    sym_patch = img[-sym_len_He:, :, :]
-    inv_idx = paddle.arange(sym_patch.shape[0] - 1, -1, -1).astype('float32')
-    sym_patch_inv = sym_patch.index_select(0, inv_idx)
-    img_aug.narrow(0, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
+    sym_patch = img.astype('float32')[-sym_len_He:, :, :]
+    inv_idx = paddle.arange(sym_patch.shape[0] - 1, -1, -1)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 0)
+    # img_aug.narrow(0, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
+    img_aug[sym_len_Hs+in_H:sym_len_He+in_H+sym_len_He,:,:] = sym_patch_inv.astype("float32")
 
     #out_1 = torch.FloatTensor(out_H, in_W, in_C)
     out_1 = paddle.zeros((out_H,in_W,in_C),paddle.float32)
     kernel_width = weights_H.shape[1]
     for i in range(out_H):
         idx = int(indices_H[i][0])
-        out_1[i, :, 0] = img_aug[idx:idx + kernel_width, :, 0].transpose(0, 1).mv(weights_H[i])
-        out_1[i, :, 1] = img_aug[idx:idx + kernel_width, :, 1].transpose(0, 1).mv(weights_H[i])
-        out_1[i, :, 2] = img_aug[idx:idx + kernel_width, :, 2].transpose(0, 1).mv(weights_H[i])
+        out_1[i, :, 0] = img_aug[idx:idx + kernel_width, :, 0].transpose([1, 0]).mv(weights_H[i])
+        out_1[i, :, 1] = img_aug[idx:idx + kernel_width, :, 1].transpose([1, 0]).mv(weights_H[i])
+        out_1[i, :, 2] = img_aug[idx:idx + kernel_width, :, 2].transpose([1, 0]).mv(weights_H[i])
 
     # process W dimension
     # symmetric copying
     #out_1_aug = torch.FloatTensor(out_H, in_W + sym_len_Ws + sym_len_We, in_C)
     out_1_aug = paddle.zeros((out_H, in_W + sym_len_Ws + sym_len_We, in_C),paddle.float32)
-    out_1_aug.narrow(1, sym_len_Ws, in_W).copy_(out_1)
+    # out_1_aug.narrow(1, sym_len_Ws, in_W).copy_(out_1)
+    out_1_aug[:,sym_len_Ws:sym_len_Ws+in_W,:] = out_1.astype("float32")
 
     sym_patch = out_1[:, :sym_len_Ws, :]
-    inv_idx = paddle.arange(sym_patch.shape[1] - 1, -1, -1).astype('float32')
-    sym_patch_inv = sym_patch.index_select(1, inv_idx)
-    out_1_aug.narrow(1, 0, sym_len_Ws).copy_(sym_patch_inv)
+    inv_idx = paddle.arange(sym_patch.shape[1] - 1, -1, -1)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 1)
+    # out_1_aug.narrow(1, 0, sym_len_Ws).copy_(sym_patch_inv)
+    out_1_aug[:,:sym_len_Ws,:] = sym_patch_inv.astype("float32")
 
     sym_patch = out_1[:, -sym_len_We:, :]
-    inv_idx = paddle.arange(sym_patch.shape[1] - 1, -1, -1).astype('float32')
-    sym_patch_inv = sym_patch.index_select(1, inv_idx)
-    out_1_aug.narrow(1, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
+    inv_idx = paddle.arange(sym_patch.shape[1] - 1, -1, -1)
+    sym_patch_inv = sym_patch.index_select(inv_idx, 1)
+    # out_1_aug.narrow(1, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
+    out_1_aug[:,sym_len_Ws+in_W:sym_len_Ws+in_W+sym_len_We,:] = sym_patch_inv.astype('float32')
 
     out_2 = paddle.zeros((out_H, out_W, in_C),paddle.float32)
     #out_2 = torch.FloatTensor(out_H, out_W, in_C)
